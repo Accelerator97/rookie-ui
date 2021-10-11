eba9c65fd817aa1f4e4daf18cfdc5fdc35058783
薛斌 <928691436@qq.com>
2021-10-12 02:30:05 +0800

    引入react

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,1 +1,7 @@
-console.log('hi')
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Button from './button'
+
+ReactDOM.render(<Button></Button>,document.body)
+console.log(React)
+console.log(ReactDOM)
--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,9 @@ module.exports = {
         library: 'rookie-UI',
         libraryTarget: 'umd',
     },
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js', '.jsx'],
+    },
     devServer: {
         historyApiFallback: true,
         inline: true,//注意：不写hot: true，否则浏览器无法自动更新；也不要写colors:true，progress:true等，webpack2.x已不支持这些
