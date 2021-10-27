0b9d5fc27af0e547dcfc898feb671a858226a7c5
薛斌 <928691436@qq.com>
2021-10-27 16:20:37 +0800

    Menu组件基本完成

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,13 +1,18 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 import Button,{ButtonType,ButtonSize} from './components/Button/button'
+import Menu from './components/Menu/menu'
+import MenuItem from './components/Menu/menuItem'
 import './styles/index.scss'
 
 
 const root = document.querySelector('#root')
 ReactDOM.render(
     <div>
-      <Button >hello</Button>
-      <Button btnType={ButtonType.Primary} size={ButtonSize.Large}>hello</Button>
-      <Button btnType={ButtonType.Link} href="http://www.baidu.com">hello</Button>
+      <Menu defaultIndex={0} onSelect={(index=>alert(index))} mode={'vertical'}>
+        <MenuItem index={0}>hi</MenuItem>
+        <MenuItem index={1} disabled>hi</MenuItem>
+        <MenuItem index={2}>hi</MenuItem>
+        <MenuItem index={3}>hi</MenuItem>
+      </Menu>
     </div>, root)
