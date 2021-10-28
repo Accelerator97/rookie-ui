3bb7452dd15e1d41d2911664430f7db6f6002b82
薛斌 <928691436@qq.com>
2021-10-28 15:24:05 +0800

    引入testing-library 完成button组件测试

--- jest.config.js
+++ jest.config.js
@@ -5,20 +5,20 @@ module.exports = {
   clearMocks: false,
   collectCoverage: false,
   reporters: ["default"],
-  moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
+  moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx','json','node'],
   moduleDirectories: ['node_modules'],
   globals: {
     'ts-jest': {
       tsConfig: 'tsconfig.test.json',
+      allowJs: true
     },
   },
   moduleNameMapper: {
     "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/test/__mocks__/file-mock.js",
   },
-  testMatch: ['<rootDir>/**/__tests__/**/*.unit.(js|jsx|ts|tsx)'],
+  testMatch: ['<rootDir>/**/__tests__/**/*.test.(js|jsx|ts|tsx)'],
   transform: {
     "^.+unit\\.(js|jsx)$": "babel-jest",
     '^.+\\.(ts|tsx)$': 'ts-jest',
-  },
-  setupFilesAfterEnv: ["<rootDir>test/setupTests.js"]
+  }
 }
---
+++ lib/__tests__/button.test.tsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import { render,fireEvent} from "@testing-library/react";
+import '@testing-library/jest-dom/extend-expect';
+import Button , { ButtonProps, ButtonSize, ButtonType }from '../components/Button/button'
+const defaultProps = {
+    onClick: jest.fn()
+}
+const testProps :ButtonProps = {
+    btnType:ButtonType.Primary,
+    size:ButtonSize.Large,
+    className:'klass'
+}
+const disabledProps:ButtonProps = {
+    disabled:true,
+    onClick:jest.fn()
+}
+describe('test Button component', () => {
+    it('should render the correct default button', () => {
+        const wrapper = render(<Button {...defaultProps}>nice</Button>)
+        const element = wrapper.getByText('nice') as HTMLButtonElement
+        expect(element).toBeInTheDocument()
+        expect(element.tagName).toEqual('BUTTON')
+        expect(element).toHaveClass('btn btn-default')
+        expect(element.disabled).toBeFalsy()
+        fireEvent.click(element)
+        expect(defaultProps.onClick).toHaveBeenCalled()
+    })
+    it('should render the correct component based on different props', () => {
+        const wrapper = render(<Button {...testProps}>nice</Button>)
+        const element = wrapper.getByText('nice')
+        expect(element).toBeInTheDocument()
+        expect(element).toHaveClass('btn-primary btn-lg klass')
+    })
+    it('should render a link when btnType equals link and href is provided', () => {
+        const wrapper = render(<Button btnType={ButtonType.Link} href="https://www.baidu.com">Link</Button>)
+        const element = wrapper.getByText('Link')
+        expect(element).toBeInTheDocument()
+        expect(element.tagName).toEqual('A')
+        expect(element).toHaveClass('btn btn-link')
+    })
+    it('should render disabled button when disabled set to true', () => {
+        const wrapper = render(<Button {...disabledProps}>disabled</Button>)
+        const element = wrapper.getByText('disabled') as HTMLButtonElement
+        expect(element).toBeInTheDocument()
+        expect(element.disabled).toBeTruthy()
+        fireEvent.click(element)
+        expect(disabledProps.onClick).not.toHaveBeenCalled()
+    })
+})
