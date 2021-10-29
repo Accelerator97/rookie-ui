450c8858089c0ab721ab7e1fb764aa4c6351e03c
薛斌 <928691436@qq.com>
2021-10-29 22:04:03 +0800

    SubMenu组件接收defaultOpenSubMenu

--- lib/components/Menu/subMenu.tsx
+++ lib/components/Menu/subMenu.tsx
@@ -9,8 +9,10 @@ export interface SubMenuProps {
 }
 
 const SubMenu:React.FC<SubMenuProps> = ({index,title,children,className}) => {
-    const [menuOpen,setOpen] = useState(false)
     const context = useContext(MenuContext)
+    const openedSubMenus = context.defaultOpenSubMenus as Array<string>
+    const isOpend = (index && context.mode ==='vertical')?openedSubMenus.includes(index):false
+    const [menuOpen,setOpen] = useState(isOpend)
     const classes = classNames('menu-item submenu-item',className,{
         'is-active' :context.index === index
     })
