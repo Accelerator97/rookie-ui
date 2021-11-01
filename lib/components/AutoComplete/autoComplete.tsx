b3476c55eeec023bcb2087dde3613a2fa959450e
薛斌 <928691436@qq.com>
2021-11-02 04:30:04 +0800

    利用泛型合并用户输入的数据类型

--- lib/components/AutoComplete/autoComplete.tsx
+++ lib/components/AutoComplete/autoComplete.tsx
@@ -1,16 +1,22 @@
 import React,{FC,useState,ChangeEvent,ReactElement} from 'react'
 import Input,{InputProps} from '../Input/input'
 
+//自定义数据类型
+interface DataSourceObject {
+    value:string;
+}
+export type DataSourceType<T = {} > = T & DataSourceObject //合并用户输入的数据类型
+
 export interface AutoCompleteProps extends Omit<InputProps,'onSelect'> {
-    fetchSuggestion:(str:string) => string[]; //用户可以自己定义，比如根据用户输入的值返回中返回符合的值
-    onSelect?:(item:string) => void,
-    renderOptions?:(item:string) => ReactElement //自定义展示模版
+    fetchSuggestion:(str:string) => DataSourceType[]; //用户可以自己定义，比如根据用户输入的值返回中返回符合的值
+    onSelect?:(item:DataSourceType) => void,
+    renderOptions?:(item:DataSourceType) => ReactElement //自定义展示模版
 }
 
 export const AutoComplete:FC<AutoCompleteProps> =(props) =>{
     const {fetchSuggestion,onSelect,value,renderOptions,...restProps} = props
     const [inputValue,setInputValue] = useState(value)//用户输入的值
-    const [suggestions,setSuggestions] = useState<string[]>([])//下拉菜单的内容
+    const [suggestions,setSuggestions] = useState<DataSourceType[]>([])//下拉菜单的内容
     const handleChange = (e:ChangeEvent<HTMLInputElement>)=>{
         const value = e.target.value.trim() //拿到用户输入的值
         setInputValue(value)
@@ -22,12 +28,12 @@ export const AutoComplete:FC<AutoCompleteProps> =(props) =>{
         }
     }
     //自定义渲染模版
-    const renderTemplate = (item:string) =>{
+    const renderTemplate = (item:DataSourceType) =>{
         return renderOptions?renderOptions(item) :item
     }
     //点击下拉菜单，把内容填充到输入框，然后下拉菜单消失，触发props的onSelect方法
-    const handleSelect = (item:string) =>{
-        setInputValue(item)
+    const handleSelect = (item:DataSourceType) =>{
+        setInputValue(item.value)
         setSuggestions([])
         if(onSelect){
             onSelect(item)
