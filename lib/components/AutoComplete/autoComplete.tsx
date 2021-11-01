efcb93a557c4c8588ecdbc49e89497a8613cb1ac
薛斌 <928691436@qq.com>
2021-11-02 06:26:33 +0800

    支持键盘事件

--- lib/components/AutoComplete/autoComplete.tsx
+++ lib/components/AutoComplete/autoComplete.tsx
@@ -1,66 +1,103 @@
-import React,{FC,useState,ChangeEvent,ReactElement,useEffect} from 'react'
-import Input,{InputProps} from '../Input/input'
+import React, { FC, useState, ChangeEvent, ReactElement, useEffect, KeyboardEvent } from 'react'
+import Input, { InputProps } from '../Input/input'
 import useDebounce from '../../hooks/useDebounce'
+import classNames from 'classnames'
 
 //自定义数据类型
 interface DataSourceObject {
-    value:string;
+    value: string;
 }
 //合并用户输入的数据类型
-export type DataSourceType<T = {} > = T & DataSourceObject 
+export type DataSourceType<T = {}> = T & DataSourceObject
 
-export interface AutoCompleteProps extends Omit<InputProps,'onSelect'> {
+export interface AutoCompleteProps extends Omit<InputProps, 'onSelect'> {
     //用户可以自己定义，比如根据用户输入的值返回中返回符合的值  同步请求返回数组或者异步请求返回数组
-    fetchSuggestion:(str:string) => DataSourceType[] | Promise<DataSourceType[]>; 
-    onSelect?:(item:DataSourceType) => void,
-    renderOption?:(item:DataSourceType) => ReactElement //自定义展示模版
+    fetchSuggestion: (str: string) => DataSourceType[] | Promise<DataSourceType[]>;
+    onSelect?: (item: DataSourceType) => void, //选中之后执行的回调
+    renderOption?: (item: DataSourceType) => ReactElement //自定义展示模版
 }
 
-export const AutoComplete:FC<AutoCompleteProps> =(props) =>{
-    const {fetchSuggestion,onSelect,value,renderOption,...restProps} = props
-    const [inputValue,setInputValue] = useState(value as string)//用户输入的值
-    const [suggestions,setSuggestions] = useState<DataSourceType[]>([])//下拉菜单的内容
-    const debouncedValue = useDebounce(inputValue,500) //函数防抖延迟更新
-    useEffect(()=>{ //只有debounceValue发生改变时才会调用函数
-        if(debouncedValue) {
+export const AutoComplete: FC<AutoCompleteProps> = (props) => {
+    const { fetchSuggestion, onSelect, value, renderOption, ...restProps } = props
+    const [inputValue, setInputValue] = useState(value as string)//用户输入的值
+    const [suggestions, setSuggestions] = useState<DataSourceType[]>([])//下拉菜单的内容
+    const [highlightIndex, setHighlightIndex] = useState(-1)
+    const debouncedValue = useDebounce(inputValue, 500) //函数防抖延迟更新
+
+    useEffect(() => { //只有debounceValue发生改变时才会调用函数
+        if (debouncedValue) {
             const results = fetchSuggestion(debouncedValue)
-            if(results instanceof Promise){ //如果result是一个promise对象
+            if (results instanceof Promise) { //如果result是一个promise对象
                 results.then(data => {
                     setSuggestions(data)
 
                 })
-            }else{ //如果result不是promise对象，是一个数组
+            } else { //如果result不是promise对象，是一个数组
                 setSuggestions(results)
             }
-        }else{
+        } else {
             setSuggestions([])
         }
-    },[debouncedValue])
-    const handleChange = (e:ChangeEvent<HTMLInputElement>)=>{
+        setHighlightIndex(-1) //重置高亮部分
+    }, [debouncedValue])
+    const hightlight = (index: number) => {
+        if (index < 0) index = 0
+        if (index >= suggestions.length) {
+            index = suggestions.length - 1
+        }
+        setHighlightIndex(index)
+    }
+    //键盘事件
+    const handleKeyDown = (e: KeyboardEvent<HTMLInputElement>) => {
+        switch (e.keyCode) {
+            case 13: //回车键
+                if(suggestions[highlightIndex]){
+                    handleSelect(suggestions[highlightIndex])
+                }
+                break
+            case 38: //上箭头
+                hightlight(highlightIndex - 1)
+                break
+            case 40: //下箭头
+                hightlight(highlightIndex + 1)
+                break
+            case 27:
+                setSuggestions([])
+                break
+            default:
+                break
+        }
+
+
+    }
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
         const value = e.target.value.trim() //拿到用户输入的值，更新useState的inputValue
-        setInputValue(value) 
+        setInputValue(value)
     }
     //自定义渲染模版
     const renderTemplate = (item: DataSourceType) => {
         return renderOption ? renderOption(item) : item.value
     }
     //点击下拉菜单，把内容填充到输入框，然后下拉菜单消失，触发props的onSelect方法
-    const handleSelect = (item:DataSourceType) =>{
+    const handleSelect = (item: DataSourceType) => {
         setInputValue(item.value)
         setSuggestions([])
-        if(onSelect){
+        if (onSelect) {
             onSelect(item)
         }
     }
     //产生下拉菜单
-    const generateDropDown = () =>{  
+    const generateDropDown = () => {
         return (
             <ul>
-                {suggestions.map((item,index)=>{
+                {suggestions.map((item, index) => {
+                    const classes = classNames('suggestion-item', {
+                        'item-hightlighted': index === highlightIndex
+                    })
                     //用户自定义的渲染模版
                     return (
-                        <li key={index} onClick={()=>handleSelect(item)}>
-                            {renderTemplate(item)} 
+                        <li key={index} className={classes} onClick={() => handleSelect(item)}>
+                            {renderTemplate(item)}
                         </li>
                     )
                 })}
@@ -69,8 +106,8 @@ export const AutoComplete:FC<AutoCompleteProps> =(props) =>{
     }
     return (
         <div className="rookie-auto-complete">
-            <Input value={inputValue} {...restProps} onChange={handleChange}></Input>
-            {suggestions.length >0 && generateDropDown()}
+            <Input value={inputValue} {...restProps} onChange={handleChange} onKeyDown={handleKeyDown}></Input>
+            {suggestions.length > 0 && generateDropDown()}
         </div>
     )
 }
