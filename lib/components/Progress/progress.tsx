33cbbac83154f54fbd4fcce614b528d176eca7d7
薛斌 <928691436@qq.com>
2021-11-04 05:51:01 +0800

    圆形进度条的样式

--- lib/components/Progress/progress.tsx
+++ lib/components/Progress/progress.tsx
@@ -1,33 +1,53 @@
-import React,{FC} from 'react'
+import React, { FC } from 'react'
 import { themeProps } from '../Icon/icon'
 
 export interface ProgressProps {
-    percent:number,
-    strokeHeight?:number,
-    showText?:boolean,
-    styles?:React.CSSProperties,
-    theme?:themeProps;
+    percent: number,
+    strokeHeight?: number,
+    showText?: boolean,
+    styles?: React.CSSProperties,
+    theme?: themeProps;
+    circle?: boolean
 }
 
-export const Progress:FC<ProgressProps> = props =>{
-    const {percent,strokeHeight,showText,styles,theme} = props
+
+
+export const Progress: FC<ProgressProps> = props => {
+    const { percent, strokeHeight, showText, styles, theme, circle } = props
+    
+
     return (
-        <div className="rookie-progress-bar" style={styles}>
-            <div className='rookie-progress-bar-outer' style={{height:`${strokeHeight}px`}}>
-                <div 
-                  className={`rookie-progress-bar-inner color-${theme}`}
-                  style={{width:`${percent}%`}}
-                > 
-                  {showText && <span className='inner-text'>{`${percent}%`}</span>}
+        <div className="rookie-progress-wrapper">
+            {circle ?
+                <div className="rookie-progress-bar" style={styles} >
+                    <div className='rookie-progress-bar-outer' style={{ height: `${strokeHeight}px` }}>
+                        <div
+                            className={`rookie-progress-bar-inner color-${theme}`}
+                            style={{ width: `${percent}%` }}
+                        >
+                            {showText && <span className='inner-text'>{`${percent}%`}</span>}
+                        </div>
+                    </div>
+                </div> :
+                <div className="rookie-progress-circle" style={styles}>
+                    <div className="rookie-progress-left" >
+                        <div className="left-circle" >
+                        </div>
+                    </div>
+                    <div className="rookie-progress-right" style={styles}>
+                        <div className="right-circle">
+                        </div>
+                    </div>
+                    {showText && <div className="innerText">{`${percent}%`}</div>}
                 </div>
-            </div>
+            }
         </div>
     )
 }
 
 Progress.defaultProps = {
-    strokeHeight:15,
-    showText:true,
-    theme:'primary'
+    strokeHeight: 15,
+    showText: true,
+    theme: 'primary'
 }
 export default Progress;
--- lib/index.tsx
+++ lib/index.tsx
@@ -1,31 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './styles/index.scss'
-import Upload,{ UploadFile } from './components/Upload/upload'
+// import Upload,{ UploadFile } from './components/Upload/upload'
+import Progress from '../lib/components/Progress/progress'
 
-const defaultFileList: UploadFile[] = [
-  { uid: '123', size: 1234, name: 'hello.md', status: 'uploading', percentage: 30 },
-  { uid: '122', size: 1234, name: 'xyz.md', status: 'uploading', percentage: 100 },
-  { uid: '121', size: 1234, name: 'eyiha.md', status: 'error', percentage: 30 }
-]
+// const defaultFileList: UploadFile[] = [
+//   { uid: '123', size: 1234, name: 'hello.md', status: 'uploading', percentage: 30 },
+//   { uid: '122', size: 1234, name: 'xyz.md', status: 'uploading', percentage: 100 },
+//   { uid: '121', size: 1234, name: 'eyiha.md', status: 'error', percentage: 30 }
+// ]
 const root = document.querySelector('#root')
-const checkFileSize =(file:File) =>{
-  if(Math.round(file.size/1024) > 50) {
-    alert('file too big')
-    return false
-  }
-  return true
-}
-const filePromise = (file:File) =>{
-  const newFile = new File([file],'new_name.md',{type:file.type})
-  return Promise.resolve(newFile)
-}
+// const checkFileSize =(file:File) =>{
+//   if(Math.round(file.size/1024) > 50) {
+//     alert('file too big')
+//     return false
+//   }
+//   return true
+// }
+// const filePromise = (file:File) =>{
+//   const newFile = new File([file],'new_name.md',{type:file.type})
+//   return Promise.resolve(newFile)
+// }
 ReactDOM.render(
   <div>
-    <Upload action="https://getman.cn/echo" 
-       defaultFileList={defaultFileList}
-       name="fileName"
-       data={{'key':'value'}}
-       header={{'x-ray':'hello'}}
-    />
+    <Progress   percent={100}/>
   </div>, root)
